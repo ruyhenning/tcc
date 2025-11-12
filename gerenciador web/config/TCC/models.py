1cad7a325f176225e1ce5c9518e782f0ebd94b00
ruy <ruyzinmelo@gmail.com>
2025-11-12 11:53:27 -0300

    tccccc

--- gerenciador web/config/TCC/models.py
+++ gerenciador web/config/TCC/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cursos.models import Cursos
 
-# Create your models here.
+
+class Ead (models.Model):
+
+    proficiências = models.CharField(max_length = 200)
+
+    descricao = models.TextField(blank=True, null=True)
+
+    data_inicio = models.DateTimeField (auto_now_add = True)
+
+    concluida = models.BooleanField(default = False)
+    Cursos = models.ForeignKey(Cursos, on_delete =models.CASCADE, null = True, blank = True)
+
+    #exibir titulo por padrão
+    def __str__(self):
+        return self.Cursos
